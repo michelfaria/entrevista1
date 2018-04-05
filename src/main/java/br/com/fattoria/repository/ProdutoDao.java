930f8aa8d60625a9a1fc147c868b98c9ec53031b
Michel <michelfariarj@gmail.com>
2018-04-05 12:22:30 -0300

    Remoção de código não utilizado

--- src/main/java/br/com/fattoria/repository/ProdutoDao.java
+++ src/main/java/br/com/fattoria/repository/ProdutoDao.java
@@ -1,16 +1,6 @@
 package br.com.fattoria.repository;
 
 import br.com.fattoria.entity.Produto;
-import org.jetbrains.annotations.Nullable;
 
 public interface ProdutoDao extends DAO<Produto> {
-    /**
-     * Busca um Produto no banco pelo nome.
-     * Se o produto for encontrado, este será retornado. Senão, retornará null.
-     *
-     * @param nome nome com o qual efetuar busca
-     * @return um Produto se for encontrado, senão null
-     */
-    @Nullable
-    Produto buscaPorNome(String nome);
 }
--- src/main/java/br/com/fattoria/repository/ProdutoDaoImpl.java
+++ src/main/java/br/com/fattoria/repository/ProdutoDaoImpl.java
@@ -1,38 +1,11 @@
 package br.com.fattoria.repository;
 
 import br.com.fattoria.entity.Produto;
-import br.com.fattoria.metamodel.Produto_;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.stereotype.Repository;
-
-import javax.persistence.NoResultException;
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Root;
 
 @Repository
 public class ProdutoDaoImpl extends JPADAO<Produto> implements ProdutoDao {
-
     public ProdutoDaoImpl() {
         super(Produto.class);
     }
-
-    @Override
-    public @Nullable
-    Produto buscaPorNome(String nome) {
-        CriteriaBuilder cb = getEntityManager().getCriteriaBuilder();
-        CriteriaQuery<Produto> cq = cb.createQuery(Produto.class);
-        Root<Produto> produto = cq.from(Produto.class);
-
-        cq.select(produto)
-                .where(cb.equal(produto.get(Produto_.nome), nome));
-
-        try {
-            return getEntityManager().createQuery(cq).getSingleResult();
-        } catch (NoResultException ex) {
-            // Caso não haja resultados, retorne null
-            return null;
-        }
-    }
-
 }
